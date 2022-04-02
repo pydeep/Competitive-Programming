299198409d7b785a1fd31587460a67ea92bd2624
pydeep <pradeep1331998@gmail.com>
2022-04-02 14:35:27 +0530

    custom label

--- missingnumber.cpp
+++ missingnumber.cpp
@@ -2,7 +2,6 @@
 {!REQUIRESCRIPT('/soap/ajax/48.0/apex.js')}
 
 var records = {!GETRECORDIDS($ObjectType.Payment__c)};
-
 var result = sforce.connection.query("SELECT Id FROM PermissionSetAssignment WHERE PermissionSet.Name IN ('Upper_Level_Payment_Processor', 'BP_Credit_Card_Recon') AND AssigneeId = '{!$User.Id}'");
 
 if(records.length<1){
@@ -18,15 +17,12 @@ else if(result.size==1){
     function cursorWait(){
         document.body.style.cursor = 'wait';
     }
-
     function cursorClear(){
         document.body.style.cursor = 'default';
     }
 
     var strQuery = "Select Id, Merchant_Name__c, Client__r.Pay_Claims_to_Parent__c, Client__r.Parent.CCD_Merchant_Name__c, Client__r.ParentId, Client__c, Client__r.Name, Client__r.CCD_Merchant_Name__c From Payment__c Where Id In("+SelectedIds+")";
-
     var queryRecord = sforce.connection.query(strQuery);
-
     var sure = confirm("{!$Label.No_Of_Selected_Payment_Records}"+' '+records.length+' '+"{!$Label.Confirm_For_Selected_Payment_Records}");
 
     if(sure){
@@ -57,7 +53,7 @@ else if(result.size==1){
                 for(var i=0; i<queryRecord.size; i++){
                     if(queryRecord.records[i].Merchant_Name__c!=null && queryRecord.records[i].Client__c!=null){
                         var IsCheck = (queryRecord.records[i].Client__r.Pay_Claims_to_Parent__c==='true');
-                        if(IsCheck) {
+                        if(IsCheck){
                             var acc = new sforce.SObject("Account");
                             acc.Id = queryRecord.records[i].Client__r.ParentId
                             acc.CCD_Merchant_Name__c = queryRecord.records[i].Merchant_Name__c;
